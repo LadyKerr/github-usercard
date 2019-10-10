4113bd06f1a640e4a98ae34c9c9c862fbecc1b6f
LadyKerr <kadiekerr@gmail.com>
2019-10-10 09:52:40 -0400

    updates to JS

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -55,77 +55,78 @@ const followersArray = [];
 */
 
 //axios call to the server
-axios.get("https://api.github.com/users/LadyKerr")
-.then(res => {
-    console.log(res)
-    const mainCard = document.querySelector(".cards")
-        mainCard.appendChild(GitHubCard(res.data))
-})
-.catch(err => console.log(err))
+axios
+  .get("https://api.github.com/users/LadyKerr")
+  .then(res => {
+    console.log(res);
+    const mainCard = document.querySelector(".cards");
+    mainCard.appendChild(GitHubCard(res.data));
+  })
+  .catch(err => console.log(err));
 
 //github card component & using the format above create elements needs to display cards
-const GitHubCard = (userData) => {
-    //card wrapper div
-    const  cardWrapper = document.createElement("div");
-    cardWrapper.classList.add('card');
+const GitHubCard = userData => {
+  //card wrapper div
+  const cardWrapper = document.createElement("div");
+  cardWrapper.classList.add("card");
 
-    //card image
-    const cardImg = document.createElement("img")
-    cardImg.src = userData.avatar_url
+  //card image
+  const cardImg = document.createElement("img");
+  cardImg.src = userData.avatar_url;
 
-    //card information wrapper div
-    const cardInfo = document.createElement("div")
-    cardInfo.classList.add("card-info")
+  //card information wrapper div
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
 
-    //user's legal name
-    const cardName = document.createElement("h3")
-    cardName.classList.add("name")
-    cardName.textContent = userData.name
+  //user's legal name
+  const cardName = document.createElement("h3");
+  cardName.classList.add("name");
+  cardName.textContent = userData.name;
 
-    //user's GH username
-    const cardUsername = document.createElement("p")
-    cardUsername.classList.add("username")
-    cardUsername.textContent = userData.login
+  //user's GH username
+  const cardUsername = document.createElement("p");
+  cardUsername.classList.add("username");
+  cardUsername.textContent = userData.login;
 
-    //user's location
-    const cardLocation = document.createElement("p")
-    cardLocation.textContent = `Location: ${userData.location}`
+  //user's location
+  const cardLocation = document.createElement("p");
+  cardLocation.textContent = `Location: ${userData.location}`;
 
-    //user's link to profile
-    const cardProfile = document.createElement("p")
-    cardProfile.textContent("Profile:")
-    //anchor tag nested inside the <p />
-    const profileLink = document.createElement("a")
-    profileLink.href = userData.html_url
-    profileLink.textContent = userData.html_url
-    cardProfile.appendChild(profileLink)
+  //user's link to profile
+  const cardProfile = document.createElement("p");
+  cardProfile.textContent = "Profile:";
+  //anchor tag nested inside the <p />
+  const profileLink = document.createElement("a");
+  profileLink.href = userData.html_url;
+  profileLink.textContent = userData.html_url;
+  cardProfile.appendChild(profileLink);
 
-    //user's followers
-    const cardFollowers = document.createElement("p")
-    cardFollowers.textContent = `Followers: ${userData.followers}`
+  //user's followers
+  const cardFollowers = document.createElement("p");
+  cardFollowers.textContent = `Followers: ${userData.followers}`;
 
-    //user's following
-    const cardFollowing = document.createElement("p")
-    cardFollowing.textContent = `Location: ${userData.following}`
+  //user's following
+  const cardFollowing = document.createElement("p");
+  cardFollowing.textContent = `Location: ${userData.following}`;
 
-    //user's bio
-    const cardBio = document.createElement("p")
-    cardBio.textContent = userData.bio
+  //user's bio
+  const cardBio = document.createElement("p");
+  cardBio.textContent = userData.bio;
 
-    //append all the elements to main wrapper div & info div to create the structure of the card
-    //main wrapper div appendChild - this is the main div that houses img & info
-    cardWrapper.appendChild(cardImg)
-    cardWrapper.appendChild(cardInfo)
+  //append all the elements to main wrapper div & info div to create the structure of the card
+  //main wrapper div appendChild - this is the main div that houses img & info
+  cardWrapper.appendChild(cardImg);
+  cardWrapper.appendChild(cardInfo);
 
-    //infoDiv appendChild - this div houses all the other divs
-    cardInfo.appendChild(cardName)
-    cardInfo.appendChild(cardUsername)
-    cardInfo.appendChild(cardLocation)
-    cardInfo.appendChild(cardProfile)
-    cardInfo.appendChild(cardFollowers)
-    cardInfo.appendChild(cardFollowing)
-    cardInfo.appendChild(cardBio)
+  //infoDiv appendChild - this div houses all the other divs
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(cardUsername);
+  cardInfo.appendChild(cardLocation);
+  cardInfo.appendChild(cardProfile);
+  cardInfo.appendChild(cardFollowers);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
 
-    //in the end return main card wrapper
-    return cardWrapper;
-}
+  //in the end return main card wrapper
+  return cardWrapper;
+};
