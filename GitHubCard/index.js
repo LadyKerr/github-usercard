b067be0b490c2e35d085fdf1fae1ae8b5fca2660
LadyKerr <kadiekerr@gmail.com>
2019-10-10 10:07:24 -0400

    changed main func to construction & added follower data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,20 +1,4 @@
-/* Step 1: using axios, send a GET request to the following URL
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
-/* Step 2: Inspect and study the data coming back, this is YOUR
-   github info! You will need to understand the structure of this
-   data in order to use it to build your component function
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function,
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either
+/* Step 5: Now that you have your own card added to the DOM, either
           follow this link in your browser https://api.github.com/users/<Your github name>/followers
           , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
@@ -24,48 +8,42 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+//adding html class to the global scope
+const mainCard = document.querySelector(".cards");
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
-/* List of LS Instructors Github username's:
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
-
-//axios call to the server
+//axios call to the server to retrieve my info
 axios
   .get("https://api.github.com/users/LadyKerr")
   .then(res => {
     console.log(res);
-    const mainCard = document.querySelector(".cards");
     mainCard.appendChild(GitHubCard(res.data));
   })
   .catch(err => console.log(err));
 
+//axios call to retrieve followers info
+axios
+  .get("https://api.github.com/users/LadyKerr/followers")
+  .then(res => {
+    console.log(res);
+    res.data.forEach(user => {
+      const followerCard = new GitHubCard(user);
+      mainCard.appendChild(followerCard);
+    });
+  })
+  .catch(err => console.log(err));
+
+//followersArray.forEach(user => {});
+
 //github card component & using the format above create elements needs to display cards
-const GitHubCard = userData => {
+function GitHubCard(userData) {
   //card wrapper div
   const cardWrapper = document.createElement("div");
   cardWrapper.classList.add("card");
@@ -129,4 +107,4 @@ const GitHubCard = userData => {
 
   //in the end return main card wrapper
   return cardWrapper;
-};
+}
